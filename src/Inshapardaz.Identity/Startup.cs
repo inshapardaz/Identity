45999c5f2e309a8122824e3a11571bd83ffef61b
Muhammad Farooq <muhammad.farooq@just-eat.com>
2017-09-10 01:04:05 +0100

    Removed migration

--- src/Inshapardaz.Identity/Startup.cs
+++ src/Inshapardaz.Identity/Startup.cs
@@ -80,7 +80,6 @@ namespace Inshapardaz.Identity
                 app.UseExceptionHandler("/Home/Error");
             }
 
-            InitializeDatabase(app);
             app.UseStaticFiles();
 
             app.UseIdentity();
@@ -110,43 +109,6 @@ namespace Inshapardaz.Identity
 
             var database = new ApplicationDbContext(optionsBuilder.Options).Database;
             database.Migrate();
-        }
-
-        private void InitializeDatabase(IApplicationBuilder app)
-        {
-            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
-            {
-                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
-
-                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
-                context.Database.Migrate();
-                if (!context.Clients.Any())
-                {
-                    foreach (var client in Config.GetClients())
-                    {
-                        context.Clients.Add(client.ToEntity());
-                    }
-                    context.SaveChanges();
-                }
-
-                if (!context.IdentityResources.Any())
-                {
-                    foreach (var resource in Config.GetIdentityResources())
-                    {
-                        context.IdentityResources.Add(resource.ToEntity());
-                    }
-                    context.SaveChanges();
-                }
-
-                if (!context.ApiResources.Any())
-                {
-                    foreach (var resource in Config.GetApiResources())
-                    {
-                        context.ApiResources.Add(resource.ToEntity());
-                    }
-                    context.SaveChanges();
-                }
-            }
         }
     }
 }
